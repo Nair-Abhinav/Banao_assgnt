3678fdec6475413f6ae574f1f420e4110b4071a6
Nair-Abhinav <abhinavnair7404@gmail.com>
2025-01-10 23:58:11 +0530

    Refactor server structure: move server logic from app.js to index.js for improved organization

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,24 @@
-const http = require('http');
-const app = require('./app');
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+const cors = require('cors');
+const connectDB = require('./DB/db.DB');
+const cookieParser = require('cookie-parser');
+const userRoutes = require('./Routes/user.routes');
 const port = process.env.PORT || 5000;
 
-const server = http.createServer(app);
+// db call
+connectDB();
 
-server.listen(port ,()=>{
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+// Routes
+app.use('/api',userRoutes);
+app.listen(port ,()=>{
     console.log(`Server is running on port ${port}`);
 });
+
